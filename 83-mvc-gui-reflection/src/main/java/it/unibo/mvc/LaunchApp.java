7c65f9e50eed611f689645cd4e2ffc646ead58eb
nicholas benedetti <nicholas.benedetti@studio.unibo.it>
2024-11-14 16:21:07 +0100

    ex-83: implemented Output Only view

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -3,6 +3,7 @@ package it.unibo.mvc;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
+import it.unibo.mvc.view.DrawNumberOutputOnlyView;
 import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
@@ -27,6 +28,6 @@ public final class LaunchApp {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
         app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberOutputOnlyView());
     }
 }
---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberOutputOnlyView.java
@@ -0,0 +1,35 @@
+package it.unibo.mvc.view;
+
+import javax.swing.JOptionPane;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberOutputOnlyView implements DrawNumberView{
+    private static final String NEW_GAME = ": a new game starts!";
+
+    @Override
+    public void setController(DrawNumberController observer) {
+        //we don't need to communicate with the controller
+    }
+
+    @Override
+    public void start() {
+        //It prints on stdout so we dont't need to initialize the UI
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        switch (res) {
+            case YOURS_HIGH, YOURS_LOW -> {
+                System.out.println(res.getDescription());
+                return;
+            }
+            case YOU_WON -> System.out.println(res.getDescription() + NEW_GAME);
+            case YOU_LOST -> System.out.println(res.getDescription() + NEW_GAME);
+            default -> throw new IllegalStateException("Unknown game state");
+        }
+    }
+
+}
